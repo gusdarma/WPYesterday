ca39329cb50d473b3b6c296c95aedd907e562723
Gus Darma <gusdarmap@gmail.com>
2020-05-19 00:44:20 +0800

    Additonal Social media settings

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -28,18 +28,24 @@ add_action('admin_menu', 'yesterday_add_admin_page');
 
 //Custom Settings for General
 function yesterday_custom_settings() {
-    register_setting( 'yesterday-setting-group', 'first_name' );
-    register_setting( 'yesterday-setting-group', 'last_name' );
-    register_setting( 'yesterday-setting-group', 'handler_twitter' );
+    // register_setting( 'yesterday-setting-group', 'first_name' );
+    // register_setting( 'yesterday-setting-group', 'last_name' );
+    register_setting( 'yesterday-setting-group', 'handler_twitter', 'yesterday_sanitize_twitter');
     register_setting( 'yesterday-setting-group', 'handler_facebook' );
     register_setting( 'yesterday-setting-group', 'handler_instagram' );
+    register_setting( 'yesterday-setting-group', 'handler_medium' );
+    register_setting( 'yesterday-setting-group', 'handler_youtube' );
 
-    add_settings_section( 'yesterday-sidebar-options', 'Sidebar Options', 'yesterday_sidebar_options', 'yesterday' );
-    add_settings_field( 'sidebar-name', 'Full Name', 'yesterday_sidebar_name', 'yesterday', 'yesterday-sidebar-options' );
-    add_settings_field( 'sidebar-name', 'Last Name', 'yesterday_sidebar_name', 'yesterday', 'yesterday-sidebar-options' );
+    add_settings_section( 'yesterday-sidebar-options', 'Social Media', 'yesterday_sidebar_options', 'yesterday' );
+    // add_settings_field( 'sidebar-name', 'Full Name', 'yesterday_sidebar_name', 'yesterday', 'yesterday-sidebar-options' );
+    // add_settings_field( 'sidebar-name', 'Last Name', 'yesterday_sidebar_name', 'yesterday', 'yesterday-sidebar-options' );
     add_settings_field( 'sidebar-twitter', 'Twitter', 'yesterday_sidebar_twitter', 'yesterday', 'yesterday-sidebar-options' );
     add_settings_field( 'sidebar-facebook', 'Facebook', 'yesterday_sidebar_facebook', 'yesterday', 'yesterday-sidebar-options' );
     add_settings_field( 'sidebar-instagram', 'Instagram', 'yesterday_sidebar_instagram', 'yesterday', 'yesterday-sidebar-options' );
+    add_settings_field( 'sidebar-medium', 'Medium', 'yesterday_sidebar_medium', 'yesterday', 'yesterday-sidebar-options' );
+    add_settings_field( 'sidebar-youtube', 'Youtube', 'yesterday_sidebar_youtube', 'yesterday', 'yesterday-sidebar-options' );
+
+
 
 }
 
@@ -48,14 +54,14 @@ function yesterday_theme_create_page() {
 }
 
 function yesterday_sidebar_options() {
-    echo '<p>Customize your sidebar here</p>';  
+    echo '<p>Input your social media details</p>';  
 }
 
-function yesterday_sidebar_name() {
-    $first_name = esc_attr( get_option( 'first_name' ) ) ;
-    $last_name = esc_attr( get_option( 'last_name' ) ) ;  
-    echo '<input type="text" name="first_name" value="'.$first_name.'" placeholder="First Name" /> <input type="text" name="last_name" value="'.$last_name.'" placeholder="Last Name" />';
-}
+// function yesterday_sidebar_name() {
+//     $first_name = esc_attr( get_option( 'first_name' ) ) ;
+//     $last_name = esc_attr( get_option( 'last_name' ) ) ;  
+//     echo '<input type="text" name="first_name" value="'.$first_name.'" placeholder="First Name" /> <input type="text" name="last_name" value="'.$last_name.'" placeholder="Last Name" />';
+// }
 
 function yesterday_sidebar_twitter() {
     $handler_twitter = esc_attr( get_option( 'handler_twitter' ) ) ;  
@@ -72,7 +78,24 @@ function yesterday_sidebar_instagram() {
     echo '<input type="text" name="handler_instagram" value="'.$handler_instagram.'" placeholder="Instagram" />' ;
 }
 
+function yesterday_sidebar_medium() {
+    $handler_medium = esc_attr( get_option( 'handler_medium' ) ) ;  
+    echo '<input type="text" name="handler_medium" value="'.$handler_medium.'" placeholder="Medium" />' ;
+}
 
+function yesterday_sidebar_youtube() {
+    $handler_yotube = esc_attr( get_option( 'handler_youtube' ) ) ;  
+    echo '<input type="text" name="handler_youtube" value="'.$handler_youtube.'" placeholder="Youtube" />' ;
+}
+
+
+//Function sanitize
+function yesterday_sanitize_twitter( $input ){
+    $output = sanitize_text_field( $input );
+    $output = str_replace( '@', '', $output);
+    return $output;
+}
+ 
 //Custom Settings for Custom CSS
 function yesterday_theme_custom_css() {
     require_once( get_template_directory() . '/inc/templates/yesterday-custom-css.php' );
--- inc/templates/yesterday-admin.php
+++ inc/templates/yesterday-admin.php
@@ -2,8 +2,105 @@
 <h1>General Settings</h1>
 <br>
 <?php settings_errors(); ?>
-<form method="post" action="options.php">
-    <?php settings_fields( 'yesterday-setting-group' );?>
-    <?php do_settings_sections( 'yesterday' ); ?>
-    <?php submit_button(); ?>
-</form>
+
+
+<style>
+.tab {
+  float: left;
+  background-color: #f1f1f1;
+  width: 20%;
+  height: 300px;
+}
+
+/* Style the butto  ns inside the tab */
+.tab button {
+  display: block;
+  background-color: inherit;
+  color: black;
+  padding: 15px 15px;
+  width: 100%;
+  border: none;
+  outline: none;
+  text-align: left;
+  cursor: pointer;
+  transition: 0.3s;
+  font-size: 14px;
+}
+
+/* Change background color of buttons on hover */
+.tab button:hover {
+  background-color: #1565c0;
+  color:white;
+}
+
+/* Create an active/current "tab button" class */
+.tab button.active {
+  background-color: #1565c0;
+  color:white;
+}
+
+/* Style the tab content */
+.tabcontent {
+  float: left;
+  padding: 0px 12px;
+  width: 65%;
+  border-left: none;
+  height: 300px;
+  padding-left:0px;
+}
+.tabcontent h2{
+    margin-top:0px!important;
+}
+.tabcontent form{
+    padding:1.5rem;
+    background-color:#f9f9f9;
+}
+</style>
+
+<div class="wrap-tab">
+    <div class="tab tab-inner">
+        <button class="tablinks" onclick="openCity(event, 'Social')" id="defaultOpen">Social Media</button>
+        <button class="tablinks" onclick="openCity(event, 'CustomCSS')">Custom CSS</button>
+        <!-- <button class="tablinks" onclick="openCity(event, 'Tokyo')">Tokyo</button> -->
+    </div>
+
+    <div id="Social" class="tabcontent tab-inner">
+        <form method="post" action="options.php">
+            <?php settings_fields( 'yesterday-setting-group' );?>
+            <?php do_settings_sections( 'yesterday' ); ?>
+            <?php submit_button(); ?>
+        </form>
+    </div>
+
+    <div id="CustomCSS" class="tabcontent tab-inner">
+       <form method="post" action="options.php">
+            <?php settings_fields( 'yesterday-setting-group' );?>
+            <?php do_settings_sections( 'yesterday' ); ?>
+            <?php submit_button(); ?>
+        </form>
+    </div>
+</div>
+
+<!-- <div id="Tokyo" class="tabcontent">
+  <h3>Tokyo</h3>
+  <p>Tokyo is the capital of Japan.</p>
+</div> -->
+
+<script>
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+</script>
